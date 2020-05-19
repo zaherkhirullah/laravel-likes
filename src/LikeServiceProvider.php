93017583f8c89b0c2b2f48d7096a363b6e743ce8
zahir hayrullah <z.hayrullah@imtilak.net>
2020-05-19 06:08:05 +0300

    create LikeServiceProvider

---
+++ src/LikeServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Hayrullah\LaravelLikes;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ *
+ * @license MIT
+ * @package Hayrullah/laravel-likes
+ *
+ * Copyright @2020 Zaher Khirullah
+ */
+class LikeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../migrations/create_likes_table.php.stub' => database_path('migrations/'.date('Y_m_d_His').'_create_likes_table.php'),
+        ], 'migrations');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
